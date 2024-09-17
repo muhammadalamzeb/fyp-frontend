fa9924460b263b5d13b2fd9d91c32cddbf19cfea
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2024-09-17 13:36:52 +0500

    ok

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL = "https://fyp-backend-i-clinic.vercel.app/api/v1";
 export const token = localStorage.getItem("token")
